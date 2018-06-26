065a6477e7245edf9f1a0b95069faafef4949760
LathaSuriya <38651177+LathaSuriya@users.noreply.github.com>
2018-06-26 11:04:24 +0000

    Rename odd.c to even.c

---
+++ begineer/even.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int j;
+    scanf("%d",&j);
+    if(j%2==0)
+    {
+        printf(" Even");
+    }
+    else
+    {
+        printf("Odd");
+    }
+
+    return 0;
+}
