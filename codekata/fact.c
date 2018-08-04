e489387ad0754914f0d5abb3fb493d195ed272ef
LathaSuriya <38651177+LathaSuriya@users.noreply.github.com>
2018-08-04 07:09:09 +0000

    Create fact.c

---
+++ codekata/fact.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int n, i;
+    unsigned long long factorial = 1;
+    scanf("%d",&n);
+    if (n < 0)
+        printf("Error.");
+
+    else
+    {
+        for(i=1; i<=n; ++i)
+        {
+            factorial *= i;             
+        }
+        printf("%llu", factorial);
+    }
+
+    return 0;
+}
