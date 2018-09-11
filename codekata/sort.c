29ddca61d383f80f5429daa7ab2f619083924d8f
LathaSuriya <38651177+LathaSuriya@users.noreply.github.com>
2018-09-11 10:34:53 +0000

    Create sort.c

---
+++ codekata/sort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+   int arr[40],n,j,i,a;
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+   {
+    scanf("%d",&arr[i]);
+   }
+   for(i=1;i<n;i++)
+   {
+        for(j=i+1;j<n;j++)
+        {
+            if(arr[i]>arr[j])
+            {
+                a=arr[i];
+                arr[i]=arr[j];
+                arr[j]=a;
+            }
+        }
+   }
+   for(i=0;i<n;i++)
+   {
+       printf("%d",arr[i]);
+   }
+   return 0;
+}
